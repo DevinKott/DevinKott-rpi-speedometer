6ab68b2a8706c52e62a51635544a6bb9eca312ab
DevinKott <devin.kott@gmail.com>
2023-11-15 20:18:53 -0500

    Update Program.cs
    Modify Program.cs to start up the RaspberryPiDriver class

--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DevinKott.RPi;
+using Microsoft.Extensions.Logging;
+
+/// Create a logging factory
+using var loggerFactory = LoggerFactory.Create(
+    builder =>
+    {
+        builder.AddFilter("Microsoft", LogLevel.Warning)
+            .AddFilter("System", LogLevel.Warning)
+            .AddConsole();
+    }
+);
+
+/// Create ILoggers
+ILogger<RaspberryPiDriver> logger = loggerFactory.CreateLogger<RaspberryPiDriver>();
+
+/// Instantiate our custom driver with a logger
+RaspberryPiDriver driver = new(logger);
+
+// Trigger: board 7, gpio 4
+// Echo: board 11, gpio 17
+await driver.Setup(7, 11);
+
+CancellationTokenSource cts = new();
+await Task.Run(() => driver.Start(cts.Token));
